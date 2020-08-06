63d73efa101fdc85c09d18a9d962c1440ffff48c
luccas <luccas.rodrigues@arquivei.com.br>
2020-08-06 11:21:42 -0300

    Ajuste para que o script do tower.bash saiba lidar com a env SPECIFIC_SERVICE_TAG

--- scripts/tower.bash
+++ scripts/tower.bash
@@ -23,7 +23,13 @@ echo $hostval
 echo $userval
 echo $VERSION
 
-# Let's run a tower-cli job
-awx --conf.host https://${hostval} --conf.username ${userval} --conf.password ${passwordval} \
--f human workflow_job_templates launch ${TEMPLATE_ID} \
---extra_vars='{"deploy_version": "'"${VERSION}"'", "deploy_name": "'"${SERVICE_NAME}"'", "commit_message": "'"${COMMIT_MESSAGE}"'"}'
+if [[ -z ${SPECIFIC_SERVICE_TAG} ]]; then
+  echo "SPECIFIC_SERVICE_TAG not set, using NEW template format WITHOUT specific tag"
+  awx --conf.host https://${hostval} --conf.username ${userval} --conf.password ${passwordval} \
+  -f human workflow_job_templates launch ${TEMPLATE_ID} \
+  --extra_vars='{"deploy_version": "'"${VERSION}"'", "deploy_name": "'"${SERVICE_NAME}"'", "commit_message": "'"${COMMIT_MESSAGE}"'"}'
+else
+  awx --conf.host https://${hostval} --conf.username ${userval} --conf.password ${passwordval} \
+  -f human workflow_job_templates launch ${TEMPLATE_ID} \
+  --extra_vars='{"deploy_version": "'"${VERSION}"'", "deploy_name": "'"${SERVICE_NAME}"'", "commit_message": "'"${COMMIT_MESSAGE}"'", "specific_service_tag": "'"${SPECIFIC_SERVICE_TAG}"'"}'
+fi
